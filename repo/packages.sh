5e978d819555308e5d850cd9b08f38794070280b
mrrafeh <rafehb@icloud.com>
2017-03-17 20:25:31 +0000

    removed old repo

--- repo/packages.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-dpkg-deb -bZgzip projects/Alpqmz001 debs
--- repo/projects/Alpqmz001/var/mobile/Library/SBHTML/alpqmz001/Config.js
+++
@@ -1,2 +0,0 @@
-//var Clock = "12h"; // choose between "12h" or "24h"
-var Lang = "en"; // choose between "en", "ca", "fr", "de", "it", "ru", "pl", "pt", "cz"
